b654adcefc97da3b7aaad47ab21cca4e63f0735a
Sene4kaaa <senkevich92@mail.ru>
2023-06-20 23:00:16 +0300

    Refactoring code

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import s from './Projects.module.css'
+import s from './Projects.module.scss'
 import styleContainer from '../../common/styles/Container.module.css'
 import {Project} from './Project/Project';
 import {Title} from '../../common/components/title/Title';
@@ -23,7 +23,7 @@ export const Projects = () => {
         <div className={s.projectsBlock}>
             <div className={`${styleContainer.container} ${s.projectsContainer} `}>
                 <Title title={'Projects'}/>
-                <div className={`${s.projects}`}>
+                <div className={s.projects}>
                     <Project style={socialNetwork} title={'Social Network'} description={'Project descriptdsav savsdvdsvsgvsgfvvgfgvf gf fg svgs  ds gdssdvdsvdsvsdvdsvdsvdsvdfbbfdfb fbfd bf fb db fd bfd fdb fbd fdb fdb fdbs  svdds vsion'}/>
                     <Project  style={todolist} title={'Todolist'} description={'Project description'}/>
                     {/*<Project title={'Portfolio'} description={'Project description'}/>*/}
