5f80625dc5b3917a9b3ae252822808eb89cc183e
Sene4kaaa <senkevich92@mail.ru>
2023-06-21 18:21:53 +0300

    Refactoring code Add svg-icon

--- src/components/Footer/Block/Block.tsx
+++ src/components/Footer/Block/Block.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import s from './Block.module.scss'
+import {StyleType} from "../../Projects/Project/Project";
+
+export type BlockPropsType = {
+    style: StyleType
+}
 
 
-export const Block = () => {
+export const Block = (props: BlockPropsType) => {
     return (
-        <div className={s.block}></div>
-    )
+        <div className={s.block} style={props.style}>
+        </div>
+
+)
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,18 +3,43 @@ import s from './Footer.module.scss'
 import {Block} from "./Block/Block";
 import styleContainer from "../../common/styles/Container.module.css";
 import {Title} from "../../common/components/title/Title";
-
+import {StyleType} from "../Projects/Project/Project";
+import instagram from "../../assets/svg-icon/instagram-1-svgrepo-com.svg";
+import telegram from "../../assets/svg-icon/telegram-logo-svgrepo-com.svg";
+import github from "../../assets/svg-icon/github-svgrepo-com.svg";
+import linkedin from "../../assets/svg-icon/linkedin-svgrepo-com.svg";
 
 export const Footer = () => {
+
+    const insta: StyleType = {
+        color: 'blue',
+        backgroundImage: `url(${instagram})`,
+    };
+
+    const telega: StyleType = {
+        color: 'blue',
+        backgroundImage: `url(${telegram})`,
+    };
+
+    const gitHub: StyleType = {
+        color: 'blue',
+        backgroundImage: `url(${github})`,
+    };
+
+    const linkIN: StyleType = {
+        color: 'blue',
+        backgroundImage: `url(${linkedin})`,
+    };
+
     return (
         <div className={s.footerBlock}>
             <div className={`${styleContainer.container} ${s.footerContainer} `}>
                 <Title title={'Andrew Senkevich'}/>
                 <div className={`${styleContainer.container} ${s.blockContainer} `}>
-                    <Block/>
-                    <Block/>
-                    <Block/>
-                    <Block/>
+                    <Block style={insta}/>
+                    <Block style={telega}/>
+                    <Block style={gitHub}/>
+                    <Block style={linkIN}/>
                 </div>
                 <span className={s.title}>© 2023 All rights reserved</span>
             </div>
