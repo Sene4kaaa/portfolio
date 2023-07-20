bbd864fe30226a76723d37eba8d7fe89cd0dc35a
Sene4kaaa <senkevich92@mail.ru>
2023-07-20 21:35:35 +0300

    Refactoring code

--- src/components/Footer/Block/Block.tsx
+++ src/components/Footer/Block/Block.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import s from './Block.module.scss'
-import {StyleType} from "../../Projects/Project/Project";
+
 
 export type BlockPropsType = {
-    style: StyleType
     src?:string
 }
 
 
 export const Block = (props: BlockPropsType) => {
     return (
-        <div className={s.block} style={props.style}>
+        <div >
             <img src={props.src} alt="" className={s.block}/>
         </div>
 
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import s from './Footer.module.scss'
 import {Block} from "./Block/Block";
 import styleContainer from "../../common/styles/Container.module.scss";
 import {Title} from "../../common/components/title/Title";
-import {StyleType} from "../Projects/Project/Project";
+
 import instagram from "../../assets/svg-icon/instagram-1-svgrepo-com.svg";
 import telegram from "../../assets/svg-icon/telegram-logo-svgrepo-com.svg";
 import github from "../../assets/svg-icon/github-svgrepo-com.svg";
@@ -11,36 +11,20 @@ import linkedin from "../../assets/svg-icon/linkedin-svgrepo-com.svg";
 
 export const Footer = () => {
 
-    const insta: StyleType = {
-        color: 'blue',
-        backgroundImage: `url(${instagram})`,
-    };
-
-    const telega: StyleType = {
-        color: 'blue',
-        backgroundImage: `url(${telegram})`,
-    };
-
-    const gitHub: StyleType = {
-        color: 'blue',
-        backgroundImage: `url(${github})`,
-    };
-
-    const linkIN: StyleType = {
-        color: 'blue',
-        backgroundImage: `url(${linkedin})`,
-    };
-
     return (
         <div className={s.footerBlock}>
             <div className={`${styleContainer.container} ${s.footerContainer} `}>
                 <Title title={'Andrew Senkevich'}/>
                 <div className={`${styleContainer.container} ${s.blockContainer} `}>
+                    <a href={'https://www.instagram.com/senkevich92_/'}>
+                        <Block src={instagram}/></a>
+                    <a href={'https://t.me/Andrew_Sene4kaa'}>
+                        <Block src={telegram}/></a>
                     <a href={'https://github.com/Sene4kaaa'}>
-                    <Block style={insta} src={instagram}/></a>
-                    <Block style={telega}/>
-                    <Block style={gitHub}/>
-                    <Block style={linkIN}/>
+                        <Block src={github}/></a>
+                    <a href={'https://ru.linkedin.com/'}>
+                        <Block src={linkedin}/></a>
+
                 </div>
                 <span className={s.title}>© 2023 All rights reserved</span>
             </div>
