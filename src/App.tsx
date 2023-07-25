f10751eba86947effbf7e14ddf1820fcab5cb7cd
Sene4kaaa <senkevich92@mail.ru>
2023-07-25 17:56:34 +0300

    Refactoring code

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import {Header} from "./components/Header/Header";
 import {Main} from "./components/Main/Main";
 import {Skills} from "./components/Skills/Skills";
 import {Projects} from "./components/Projects/Projects";
-import {RemoteWork} from "./components/Remote work/RemoteWork";
 import {Contacts} from "./components/Contacts/Contacts";
 import {Footer} from "./components/Footer/Footer";
 
