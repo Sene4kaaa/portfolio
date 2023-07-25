e2153e7e81af4ad7a0c33a6326e95fa3449099e1
Sene4kaaa <senkevich92@mail.ru>
2023-07-25 16:37:58 +0300

    Refactoring code

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -10,41 +10,46 @@ export const Contacts = () => {
     return (
         <div id={'Contacts'} className={s.contactsBlock}>
             <Fade direction={"top-left"} duration={1500}  /*triggerOnce={true}*/>
-            <div className={s.contactsContainer}>
-                <div className={s.block}>
-                    <Title title={'Let\'s get in touch'}/>
-                    <div className={s.blabla}>
-                    <p>
-                        Thank you for visiting my portfolio page! If you are looking for an <span className={s.write}>purposeful and talented front-end
-                        developer</span> who can help you create a unique and functional web interface, then you are in the right
-                        place.
-                    </p>
-                    <p>
-                        If you have any questions, comments, or proposals, please do not hesitate to contact me through the
-                        contact form. I will be happy to discuss all the details.
-                    </p>
-                    <h3 className={s.write}>Write:</h3>
-                    <h4>senkevich92@mail.ru</h4>
+                <div className={s.contactsContainer}>
+                    <div className={s.block}>
+                        <Title title={'Let\'s get in touch'}/>
+                        <div className={s.blabla}>
+                            <p>
+                                Thank you for visiting my portfolio page! If you are looking for an <span
+                                className={s.write}>purposeful and talented front-end
+                        developer</span> who can help you create a unique and functional web interface, then you are in
+                                the right
+                                place.
+                            </p>
+                            <p>
+                                If you have any questions, comments, or proposals, please do not hesitate to contact me
+                                through the
+                                contact form. I will be happy to discuss all the details.
+                            </p>
+                            <h3 className={s.write}>Write:</h3>
+                            <h4>senkevich92@mail.ru</h4>
+                        </div>
                     </div>
-                </div>
 
-                <div  className={s.block}>
-                    <Title title={'Contact'}/>
-                    <div className={s.formAll}>
-                    <form className={s.form}>
-                        <label className={s.name} htmlFor="name">What is Your Name:</label>
-                        <input type={"text"} className={s.input} name={'name'} placeholder={'Name'}/>
-                        <label className={s.name} htmlFor="email">E-mail:</label>
-                        <input type={"email"} className={s.input}  name={'name'} placeholder={'Email'}/>
-                        <label className={s.name} htmlFor="message">Message:</label>
-                        <textarea className={s.textarea} name={'message'} rows={3} placeholder={'Your message'}></textarea>
-                        <button type={"submit"} className={s.button}>Send</button>
-                    </form>
+                    <div className={s.block}>
+                        <Title title={'Contact'}/>
+                        <div className={s.formAll}>
+                            <form className={s.form}>
+                                <label className={s.name} htmlFor="name">What is Your Name:</label>
+                                <input type={"text"} className={s.input} name={'name'} placeholder={'Name'} required/>
+                                <label className={s.name} htmlFor="email">E-mail:</label>
+                                <input type={"email"} className={s.input} name={'email'} placeholder={'Email'}
+                                       required/>
+                                <label className={s.name} htmlFor="message">Message:</label>
+                                <textarea className={s.textarea} name={'message'} rows={3} placeholder={'Your message'}
+                                          required></textarea>
+                                <button type={"submit"} className={s.button}>Send</button>
+                            </form>
+                        </div>
                     </div>
+
+
                 </div>
-
-
-            </div>
             </Fade>
         </div>
     )
