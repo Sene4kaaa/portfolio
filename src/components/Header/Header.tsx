2c1326455fb2d105cf3350b87ea2c4d0dea2be0d
Sene4kaaa <senkevich92@mail.ru>
2023-07-25 17:57:00 +0300

    Refactoring code

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import s from './Header.module.scss'
 import {Nav} from "../Nav/Nav";
-import {Block} from "../Footer/Block/Block";
-import instagram from "../../assets/svg-icon/instagram-1-svgrepo-com.svg";
-import telegram from "../../assets/svg-icon/telegram-logo-svgrepo-com.svg";
-import github from "../../assets/svg-icon/github-svgrepo-com.svg";
-import linkedin from "../../assets/svg-icon/linkedin-svgrepo-com.svg";
+
 
 
 export const Header = () => {
