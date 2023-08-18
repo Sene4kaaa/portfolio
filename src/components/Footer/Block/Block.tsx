1caa9dba6b6e337fa9b3d4873f7202d42792db22
Sene4kaaa <senkevich92@mail.ru>
2023-08-18 16:25:51 +0300

    Refactoring code

--- src/components/Footer/Block/Block.tsx
+++ src/components/Footer/Block/Block.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import s from './Block.module.scss'
 
-
 export type BlockPropsType = {
-    src?:string
+    src?: string
 }
-
 
 export const Block = (props: BlockPropsType) => {
     return (
         <div className={s.blochHover}>
             <img src={props.src} alt="" className={s.block}/>
         </div>
-
-)
+    )
 }
